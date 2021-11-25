cb7cba8dad4a5e510d37d0c10f6fcb0f80b21a2b
juliantoro91 <juliandavidtoromedina@gmail.com>
2021-11-25 12:41:47 -0500

    App Structure and components add, without styles

--- example-two/src/App.js
+++ example-two/src/App.js
@@ -1,8 +1,42 @@
 // import './App.css';
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
+
+const todos = [
+  {
+    text: 'Nota de prueba 1',
+    completed: false
+  },
+  {
+    text: 'Nota de prueba 2',
+    completed: false
+  },
+  {
+    text: 'Nota de prueba 3',
+    completed: false
+  },
+];
 
 function App() {
   return (
-    
+    <>
+      <TodoCounter />
+      
+      <TodoSearch />
+      
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text}/>
+        ))}
+        <TodoItem />
+      </TodoList>
+
+      <CreateTodoButton />
+      
+    </>
   );
 }
 
